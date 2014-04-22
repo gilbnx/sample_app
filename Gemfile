786edb06a183ca111a98d9c0efe54f9c9cab84d6
Gil B <gil@phalcor.(none)>
2014-04-21 23:31:44 -0700

    added sites layouts and variables

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ group :development do
 end
 
 group :test do
+	gem 'webrat'
 	gem 'rspec-rails'
 end
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def home
+  	@title = "Home page"
   end
 
   def contact
+  	@title = "contact page"
+  end
+
+  def about
+  	@title = "about page"
   end
 end
